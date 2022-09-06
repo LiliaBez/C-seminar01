20a931600cc8466701f0a0ead53b0fa9e6de7caa
Lilia Bezrukova <lilia_dyatlova@mail.ru>
2022-09-06 21:40:12 +0400

    Zadacha00

--- 01/Program.cs
+++ 01/Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число");
+string input = Console.ReadLine(); // пользовательский ввод
+int number = int.Parse(input); //если ошибка, программа вылетает, более нежная команда if (int.TryParse (input))
+int square = number * number; // квадрат
+Console.WriteLine($"Число в квадрате равно {square}");
+// другой вариант Console.WriteLine("Число в квадрате равно {0}", square);
