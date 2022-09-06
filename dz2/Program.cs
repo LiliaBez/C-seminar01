144a90deb78c2e71f66e95cdef91441595610a12
Lilia Bezrukova <lilia_dyatlova@mail.ru>
2022-09-06 23:37:03 +0400

    Homework 2

---
+++ dz2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число A");
+string input = Console.ReadLine(); // пользовательский ввод
+Console.WriteLine("Введите число B");
+string input2 = Console.ReadLine(); // пользовательский ввод
+
+int FirstNumber = int.Parse(input);
+int SecondNumber = int.Parse(input2);
+
+if (FirstNumber == SecondNumber)
+{
+    Console.WriteLine("Эти числа равны");
+}
+else if (FirstNumber > SecondNumber)
+{
+    Console.WriteLine($"Число {FirstNumber} большее, число {SecondNumber} меньшее");
+}
+else
+{
+    Console.WriteLine($"Число {SecondNumber} большее, число {FirstNumber} меньшее");
+}
