99f4996e83b876dcf1b1e20bff1953b375121103
Lilia Bezrukova <lilia_dyatlova@mail.ru>
2022-09-07 00:53:28 +0400

    Homework 4

---
+++ dz4/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Введите число A");
+string input = Console.ReadLine(); // пользовательский ввод
+Console.WriteLine("Введите число B");
+string input2 = Console.ReadLine(); // пользовательский ввод
+Console.WriteLine("Введите число C");
+string input3 = Console.ReadLine(); // пользовательский ввод
+
+int FirstNumber = int.Parse(input);
+int SecondNumber = int.Parse(input2);
+int ThirdNumber = int.Parse(input3);
+
+if (FirstNumber > SecondNumber)
+{
+    if (FirstNumber > ThirdNumber)
+    {
+        Console.WriteLine($"Максимальное число {FirstNumber}");
+    }
+    else
+    {
+        Console.WriteLine($"Максимальное число {ThirdNumber}");
+    }
+}
+else if (SecondNumber > ThirdNumber)
+{
+    Console.WriteLine($"Максимальное число {SecondNumber}");
+}
+else
+{
+    Console.WriteLine($"Максимальное число {ThirdNumber}");
+}
+
+
+
--- z1/Program.cs
+++ z1/Program.cs
@@ -3,16 +3,16 @@ string input = Console.ReadLine(); // пользовательский ввод
 Console.WriteLine("Введите число B");
 string input2 = Console.ReadLine(); // пользовательский ввод
 
-int numberA = int.Parse(input); 
-int numberB = int.Parse(input2); 
+int numberA = int.Parse(input);
+int numberB = int.Parse(input2);
 
 
 double quot = numberA / numberB; // частное
-if (quot<=numberB)
+if (quot <= numberB)
 {
-Console.WriteLine($"Число {numberA} является квадратом числа {numberB}");
+    Console.WriteLine($"Число {numberA} является квадратом числа {numberB}");
 }
-else 
+else
 {
     Console.WriteLine($"Число {numberA} не является квадратом числа {numberB}");
 }
