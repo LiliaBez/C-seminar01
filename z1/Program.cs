b6947665f4d5abb3180a5b20afb7c454d8df078b
Lilia Bezrukova <lilia_dyatlova@mail.ru>
2022-09-06 22:12:06 +0400

    Zadacha01

--- z1/Program.cs
+++ z1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число A");
+string input = Console.ReadLine(); // пользовательский ввод
+Console.WriteLine("Введите число B");
+string input2 = Console.ReadLine(); // пользовательский ввод
+
+int numberA = int.Parse(input); //если ошибка, программа вылетает, более нежная команда if (int.TryParse (input))
+int numberB = int.Parse(input2); //если ошибка, программа вылетает, более нежная команда if (int.TryParse (input))
+
+
+double quot = numberA / numberB; // частное
+if (quot<=numberB)
+{
+Console.WriteLine($"Число {numberA} является квадратом числа {numberB}");
+}
+else 
+{
+    Console.WriteLine($"Число {numberA} не является квадратом числа {numberB}");
+}
+
