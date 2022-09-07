b765d2db27228120a3d6cb7a21e5469870bb3d44
Lilia Bezrukova <lilia_dyatlova@mail.ru>
2022-09-07 13:59:03 +0400

    Добавилаописание задачи

--- dz2/Program.cs
+++ dz2/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Введите число A");
+﻿// Напишите программу, которая на вход принимает два числа 
+//и выдает, какое число большее, а какое меньшее.
+Console.WriteLine("Введите число A");
 string input = Console.ReadLine(); // пользовательский ввод
 Console.WriteLine("Введите число B");
 string input2 = Console.ReadLine(); // пользовательский ввод
