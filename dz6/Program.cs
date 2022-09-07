b61a7cb66f3a7d06ee772bc57190f5d2c34ec91b
Lilia Bezrukova <lilia_dyatlova@mail.ru>
2022-09-07 17:08:55 +0400

    Homework 6

---
+++ dz6/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число и 
+//выдает, является ли число четным.
+Console.WriteLine("Введите число:");
+string input = Console.ReadLine();
+
+int number = int.Parse(input); 
+
+if (number%2 ==0)
+{
+    Console.WriteLine($"Число {number} четное");
+}
+else
+{
+    Console.WriteLine($"Число {number} не четное");
+}
