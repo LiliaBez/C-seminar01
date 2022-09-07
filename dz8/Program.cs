09321921c775a82e91730cbab3cb45444be527b9
Lilia Bezrukova <lilia_dyatlova@mail.ru>
2022-09-07 18:30:20 +0400

    Homework 8

---
+++ dz8/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все четные числа от 1 до N
+
+Console.WriteLine("Введите число: ");
+string input = Console.ReadLine();
+
+int number = int.Parse(input);
+
+Console.WriteLine($"Ряд четных чисел от 1 до {number}: ");
+for (int i = 2; i <= number; i = i+2)
+{
+    Console.Write(i);
+    Console.Write(' ');
+}
